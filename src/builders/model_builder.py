ef3cadfd233e36e789089ee3d3e9b4828d43dc60
Armin Saadat <armin2saadat@gmail.com>
2022-12-14 00:55:17 -0800

    debug

--- src/builders/model_builder.py
+++ src/builders/model_builder.py
@@ -38,6 +38,8 @@ def build(config: dict,
             con = deepcopy(config["attention_encoder"])
             con.update({'num_frames': config["attention_encoder"]['num_frames']//config["graph_regressor"]['agg_num']})
             con.update({'input_dim': config["graph_regressor"]['gnn_hidden_dims'][-1]})
+            print(con['input_dim'])
+            print('*' * 100)
             model[model_key] = MODELS[model_key](config=con).to(device)
         elif model_key == "graph_regressor2":
             con = deepcopy(config["graph_regressor"])
