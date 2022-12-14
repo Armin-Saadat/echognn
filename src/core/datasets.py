44996d0c87fcac659fbdd891dd623a6b4dd5d69b
Armin Saadat <armin2saadat@gmail.com>
2022-12-14 15:06:10 -0800

    debug

--- src/core/datasets.py
+++ src/core/datasets.py
@@ -177,7 +177,6 @@ class EchoNetEfDataset(Dataset, ABC):
 
         # Create fully connected graph
         nx_graph = nx.complete_graph(self.num_frames * 2, create_using=nx.DiGraph())
-        print(CH2_vid.shape)
         for i in range(1, CH2_vid.shape[0]):
             nx_graph = nx.compose(nx_graph, nx.complete_graph(range(i * self.num_frames * 2,
                                                                     (i + 1) * self.num_frames * 2),
